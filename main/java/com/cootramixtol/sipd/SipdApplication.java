1300b42e4cf0dab420a222a80d08c71b7a748320
JoseVides17 <jose.videsbaron@gmail.com>
2023-11-08 10:22:05 -0500

    commit

---
+++ main/java/com/cootramixtol/sipd/SipdApplication.java
@@ -0,0 +1,13 @@
+package com.cootramixtol.sipd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  
+public class SipdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipdApplication.class, args);
+	}
+
+}
